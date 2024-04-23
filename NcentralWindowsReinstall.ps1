90068a36309bccbea32e64e74ab1972b8fe5797a
lo-zhero <124682522+Louis-Zhero@users.noreply.github.com>
2024-04-23 15:48:09 +0100

    feat(Update NcentralWindowsReinstall.ps1)
    Included a wait then kill process for uninstallation with closure of the pop-up from the uninstaller (to circumvent the associated bug it produces)

--- NcentralWindowsReinstall.ps1
+++ NcentralWindowsReinstall.ps1
@@ -3,15 +3,32 @@
 
     Script which reinstalls N-central for windows machines. This script requires certain variables to work including, URL, CLIENTID, APIURL & JWT
 #>
-# First determine if N-central is on the asset or not
+# First determine if N-central is on the asset or not, wait 2 minutes then stop process & continue
 $NCentralStatus = Get-WmiObject -Class Win32_Product | Where-Object { $_.Name -like "*Windows Agent*" }
 $NCentralCode=$NCentralStatus.IdentifyingNumber
 if($NCentralCode){
-    Write-Host "N-central found is installed, removing now..."
-    Start-Process "msiexec.exe" -ArgumentList "/x $NCentralCode /qn" -Wait
-    Write-Host "[SUCCESS] Removal complete."
+    $AgentRemovalProcess = Start-Process "msiexec.exe" -ArgumentList "/x $NCentralCode /qn" -PassThru
+    if ($AgentRemovalProcess) {
+        # Wait for 2 minutes, if the windows agent process is still running, kill it and continue the process.
+        if ($AgentRemovalProcess.WaitForExit(120000)) {
+            Write-Host "[SUCCESS] Removal complete."
+        } else {
+            $AgentRemovalProcess.Kill()
+            Write-Host "[CONTINUE] Stopped uninstallation and continuing."
+            # Now close the Uninstall window if present - solving a bug N-able's uninstaller has
+            $BugFix = Get-Process -Name "unins000" -ErrorAction SilentlyContinue
+            if ($BugFix) {
+                # Stop the process
+                Stop-Process -Id $BugFix.Id -Force
+                Write-Host "[SUCCESS] Process 'unins000.exe' stopped successfully."
+            } else {
+                Write-Host "[SKIP] Process 'unins000.exe' not found."
+            }
+        }
+    } else {
+        Write-Host "Failed to start the removal process."
+    }
 }
-
 # Now install N-central again but to your server
 if (-not (Test-Path -Path "C:\AutomateResources")) {
     New-Item -ItemType Directory -Path "C:\AutomateResources" | Out-Null
